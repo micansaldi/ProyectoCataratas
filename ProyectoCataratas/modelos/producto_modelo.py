355c18da5c8bde3fbbb84834b4db8f78a7e7b8da
Micaela Ansaldi <micansaldi@gmail.com>
2024-07-01 19:44:37 -0300

    1000var

--- ProyectoCataratas/modelos/producto_modelo.py
+++ ProyectoCataratas/modelos/producto_modelo.py
@@ -9,7 +9,7 @@ class Producto(db.Model):   # la clase Producto hereda de db.Model
     nombre=db.Column(db.String(100))
     precio=db.Column(db.Integer)
     duracion=db.Column(db.Integer)
-    descripcion = db.Column(db.String(100))
+    descripcion = db.Column(db.String(1000))
     imagen=db.Column(db.String(400))
     def __init__(self,nombre,precio,duracion,descripcion,imagen):   #crea el  constructor de la clase
         self.nombre=nombre   # no hace falta el id porque lo crea sola mysql por ser auto_incremento
